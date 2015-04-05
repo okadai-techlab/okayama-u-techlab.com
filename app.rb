94235a86e890eb9c61f070482116ccc147253399
totz <yuta.totz@gmail.com>
2015-04-05 20:23:40 +0900

    Sinatraを導入。メール送信機能追加

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'pony'
 
 get '/' do
   erb :index
+end
+
+post '/' do
+  Pony.mail(:to => 'yuta.totz@gmail.com', :from => 'me@example.com', :subject => 'hi', :body => 'Hello there.', :via => :sendmail)
+  redirect '/'
 end
