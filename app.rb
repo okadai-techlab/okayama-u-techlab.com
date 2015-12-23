01fbc4a6da5c8adb3d914641b9169b057ba95ed5
Tagami Daiki <toda0310@gmail.com>
2015-12-24 04:37:26 +0900

    Slack通知を追加

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'sinatra/config_file'
 require 'pony'
+require 'slack'
 
 # Load yml setting file
 config_file 'config/settings.yml'
@@ -10,19 +11,21 @@ get '/' do
   erb :index
 end
 
-# メールを送信
+# メールを送信 & Slack通知
 post '/' do
   name = params[:name]
   email_address = params[:email]
   message=params[:message]
 
+  EMAIL_ADDRESS = settings.email
+  EMAIL_SECRET = settings.secret
+  SLACK_TOKEN = settings.slack_token
+  SLACK_CHANNEL = settings.slack_channel
+
   # 有効なメールアドレスかを確認する正規表現
   valid_address = /\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^\/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z/
 
   if valid_address =~ email_address 
-
-    EMAIL_ADDRESS = settings.email
-    EMAIL_SECRET = settings.secret
 
     @isSuccessSendingEmail = 
       Pony.mail(:to => EMAIL_ADDRESS,
@@ -48,4 +51,14 @@ post '/' do
     @isSuccessSendingEmail = false;
     erb :index
   end
+
+  # Slack通知設定
+  Slack.configure do |config|
+    config.token = SLACK_TOKEN
+  end
+
+  Slack.chat_postMessage(text: 
+    "てくらぼホームページからメッセージがありました＾＾
+  --------------------
+  "+ message, channel: SLACK_CHANNEL, username: 'OTL HP')
 end
